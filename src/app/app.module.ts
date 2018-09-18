fa173751c7b4e246ace25149646d7d46ec7b0c51
frankcumaio <frankcumaio@gmail.com>
2018-09-18 17:21:18 +0200

    Mapa fase 1 Usando HighMaps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HighchartsChartModule } from 'highcharts-angular';
 import { AppComponent } from './app.component';
+import {ConfigService} from './utils/config.service';
+import {DashboardsService} from './Shared/dashboards.service';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacaoComponent,
   ],
   imports: [
     BrowserModule,
-      HighchartsChartModule
+      HighchartsChartModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      LayoutModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatSidenavModule,
+      MatIconModule,
+      MatListModule,
+      MatCardModule,
+      MatGridListModule
   ],
-  providers: [],
+  providers: [DashboardsService,
+      ConfigService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
